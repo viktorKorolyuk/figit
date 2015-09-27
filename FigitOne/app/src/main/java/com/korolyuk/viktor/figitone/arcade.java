3a1a088639461467629a4cbf999435000c33f827
ViktorKorolyuk <viktor.korolyuk@gmail.com>
2015-09-27 17:50:44 -0400

    updated array code cheker to use ints, and more

--- FigitOne/app/src/main/java/com/korolyuk/viktor/figitone/arcade.java
+++ FigitOne/app/src/main/java/com/korolyuk/viktor/figitone/arcade.java
@@ -33,7 +33,7 @@ public class arcade extends ActionBarActivity {
     boolean bu3Clicked = false;
 
 
-    String[] code = {"1,2,3"};
+    String[] code = {"1,2,3","3,2,1"};
     String code1temp = code[new Random().nextInt(code.length)];
     String[] code1Decode = code1temp.split(",");
     final String codes = code1Decode.toString();
@@ -48,7 +48,7 @@ public class arcade extends ActionBarActivity {
 
         String code1temp = code[new Random().nextInt(code.length)]; //choose code
         String[] code1Decode = code1temp.split(","); //split the code into seperate objects
-        final String codes = code1Decode.toString(); // turn into strings
+        String codes = code1Decode.toString(); // turn into strings
         String code11 = code1Decode[0];
         String code12 = code1Decode[1];
         String code13 = code1Decode[2];
@@ -136,22 +136,18 @@ public class arcade extends ActionBarActivity {
                     bu3Clicked = true;
                     if (bu1One == 0) {
                         bu1One = 3;
-                        Toast.makeText(getApplicationContext(), "Bu1One == 1", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "Bu1One == 3", Toast.LENGTH_SHORT).show();
                     }else if (bu1One > 0 && bu2One == 0) {
                         bu2One = 3;
-                        Toast.makeText(getApplicationContext(), "Bu2One == 1", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "Bu2One == 3", Toast.LENGTH_SHORT).show();
                     }else if (bu1One > 0 && bu2One > 0 && bu3One == 0) {
                         bu3One = 3;
 
                     }
-
-
                     if (bu1One > 0 && bu2One > 0 && bu3One > 0) {
                         //// TODO: 15-09-25 Make function that will deal with checking if the code entered is correct.
                         checkCode();
-
                     }
-
                 } else {
 //// TODO: 15-09-25 make it so that when the user clicks same button then the code resets with alert
                 }
@@ -189,21 +185,47 @@ public class arcade extends ActionBarActivity {
 
     public void checkCode() {
 
+        int x = Integer.parseInt(code11);
+        int y = Integer.parseInt(code12);
+        int z = Integer.parseInt(code13);
+        if (bu1One == x && bu2One == y && bu3One == z) {
+            Toast.makeText(getApplicationContext(), "Congrats you got the code 1,2,3 correct!, the code will now be reset so that you can find another one!", Toast.LENGTH_SHORT).show();
+            resetCode();
+        }
+/**
         if (code1temp == "1,2,3") {
             int supo1 = 1;
             int supo2 = 2;
             int supo3 = 3;
 
             if (bu1One == supo1 && bu2One == supo2 && bu3One == supo3) {
-                Toast.makeText(getApplicationContext(), "Congrats you got the code correct!", Toast.LENGTH_SHORT).show();
-
-            } else {
-
-            }
-
-
+                Toast.makeText(getApplicationContext(), "Congrats you got the code 1,2,3 correct!, the code will now be reset so that you can find another one!", Toast.LENGTH_SHORT).show();
+resetCode();
+            } else {}
         }
 
+        if (code1temp == "3,2,1") {
+            int supo1 = 3;
+            int supo2 = 2;
+            int supo3 = 1;
+
+            if (bu1One == supo1 && bu2One == supo2 && bu3One == supo3) {
+                Toast.makeText(getApplicationContext(), "Congrats you got the code 3,2,1 correct!", Toast.LENGTH_SHORT).show();
+
+            } else {
+            }
+        }
+*/
+
+    }
+
+    public void resetCode() {
+        bu1One = 0;
+        bu2One = 0;
+        bu3One = 0;
+        bu1Clicked = false;
+        bu2Clicked = false;
+        bu3Clicked = false;
     }
 
 }

389451ef605f1dc4560b0460f37d4a347a16ac49
ViktorKorolyuk <viktor.korolyuk@gmail.com>
2015-10-01 18:03:58 -0400

    updated array code cheker to use ints, and more

--- FigitOne/app/src/main/java/com/korolyuk/viktor/figitone/arcade.java
+++ FigitOne/app/src/main/java/com/korolyuk/viktor/figitone/arcade.java
@@ -33,25 +33,24 @@ public class arcade extends ActionBarActivity {
     boolean bu3Clicked = false;
 
 
-    String[] code = {"1,2,3","3,2,1"};
+
+
+    String[] code = {"1,2,3","3,2,1","1,3,2"};
     String code1temp = code[new Random().nextInt(code.length)];
     String[] code1Decode = code1temp.split(",");
-    final String codes = code1Decode.toString();
+    String codes = code1Decode.toString();
     String code11 = code1Decode[0];
     String code12 = code1Decode[1];
     String code13 = code1Decode[2];
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.arcade);
+choosePass();
 
-        String code1temp = code[new Random().nextInt(code.length)]; //choose code
-        String[] code1Decode = code1temp.split(","); //split the code into seperate objects
-        String codes = code1Decode.toString(); // turn into strings
-        String code11 = code1Decode[0];
-        String code12 = code1Decode[1];
-        String code13 = code1Decode[2];
         final Button bu1 = (Button) findViewById(R.id.bu1);
         final Button bu2 = (Button) findViewById(R.id.bu2);
         final Button bu3 = (Button) findViewById(R.id.bu3);
@@ -68,26 +67,27 @@ public class arcade extends ActionBarActivity {
             public void onClick(View view) {
 
                 if (!bu1Clicked) {
-                    bu1Clicked = true;
-                    if (bu1One == 0) {
-                        bu1One = 1;
-                        Toast.makeText(getApplicationContext(), "Bu1One == 1", Toast.LENGTH_SHORT).show();
-                    }else if (bu1One > 0 && bu2One == 0) {
-                        bu2One = 1;
-                        Toast.makeText(getApplicationContext(), "Bu2One == 1", Toast.LENGTH_SHORT).show();
-                    }else if (bu1One > 0 && bu2One > 0 && bu3One == 0) {
-                        bu3One = 1;
+               //     bu1Clicked = true;
 
-                    }
-
-                    if (bu1One > 0 && bu2One > 0 && bu3One > 0) {
-                        //// TODO: 15-09-25 Make function that will deal with checking if the code entered is correct.
-                        checkCode();
-                    }
 
                 } else {
 //// TODO: 15-09-25 make it so that when the user clicks same button then the code resets with alert
                     //// TODO: 15-09-26 OPTIONAL OBVIOUSLY
+                }
+                if (bu1One == 0) {
+                    bu1One = 1;
+                    //     Toast.makeText(getApplicationContext(), "Bu1One == 1", Toast.LENGTH_SHORT).show();
+                }else if (bu1One > 0 && bu2One == 0) {
+                    bu2One = 1;
+                    //     Toast.makeText(getApplicationContext(), "Bu2One == 1", Toast.LENGTH_SHORT).show();
+                }else if (bu1One > 0 && bu2One > 0 && bu3One == 0) {
+                    bu3One = 1;
+                    //    Toast.makeText(getApplicationContext(), "Bu3One == 2", Toast.LENGTH_SHORT).show();
+                }
+
+                if (bu1One > 0 && bu2One > 0 && bu3One > 0) {
+                    //// TODO: 15-09-25 Make function that will deal with checking if the code entered is correct.
+                    checkCode();
                 }
 
 
@@ -98,26 +98,28 @@ public class arcade extends ActionBarActivity {
             public void onClick(View view) {
 
                 if (!bu2Clicked) {
-                    bu2Clicked = true;
-                    if (bu1One == 0) {
-                        bu1One = 2;
-                        Toast.makeText(getApplicationContext(), "Bu1One == 1", Toast.LENGTH_SHORT).show();
-                    }else if (bu1One > 0 && bu2One == 0) {
-                        bu2One = 2;
-                        Toast.makeText(getApplicationContext(), "Bu2One == 1", Toast.LENGTH_SHORT).show();
-                    }else if (bu1One > 0 && bu2One > 0 && bu3One == 0) {
-                        bu3One = 2;
 
-                    }
-
-                    if (bu1One > 0 && bu2One > 0 && bu3One > 0) {
-                        //// TODO: 15-09-25 Make function that will deal with checking if the code entered is correct.
-                        checkCode();
-
-                    }
 
                 } else {
 //// TODO: 15-09-25 make it so that when the user clicks same button then the code resets with alert
+                }
+
+            //    bu2Clicked = true;
+                if (bu1One == 0) {
+                    bu1One = 2;
+                    //      Toast.makeText(getApplicationContext(), "Bu1One == 2", Toast.LENGTH_SHORT).show();
+                }else if (bu1One > 0 && bu2One == 0) {
+                    bu2One = 2;
+                    //     Toast.makeText(getApplicationContext(), "Bu2One == 2", Toast.LENGTH_SHORT).show();
+                }else if (bu1One > 0 && bu2One > 0 && bu3One == 0) {
+                    bu3One = 2;
+                    //     Toast.makeText(getApplicationContext(), "Bu3One == 2", Toast.LENGTH_SHORT).show();
+                }
+
+                if (bu1One > 0 && bu2One > 0 && bu3One > 0) {
+                    //// TODO: 15-09-25 Make function that will deal with checking if the code entered is correct.
+                    checkCode();
+
                 }
 
 
@@ -133,25 +135,26 @@ public class arcade extends ActionBarActivity {
             public void onClick(View view) {
 
                 if (!bu3Clicked) {
-                    bu3Clicked = true;
-                    if (bu1One == 0) {
-                        bu1One = 3;
-                        Toast.makeText(getApplicationContext(), "Bu1One == 3", Toast.LENGTH_SHORT).show();
-                    }else if (bu1One > 0 && bu2One == 0) {
-                        bu2One = 3;
-                        Toast.makeText(getApplicationContext(), "Bu2One == 3", Toast.LENGTH_SHORT).show();
-                    }else if (bu1One > 0 && bu2One > 0 && bu3One == 0) {
-                        bu3One = 3;
+              //      bu3Clicked = true;
 
-                    }
-                    if (bu1One > 0 && bu2One > 0 && bu3One > 0) {
-                        //// TODO: 15-09-25 Make function that will deal with checking if the code entered is correct.
-                        checkCode();
-                    }
                 } else {
 //// TODO: 15-09-25 make it so that when the user clicks same button then the code resets with alert
                 }
 
+                if (bu1One == 0) {
+                    bu1One = 3;
+                    //    Toast.makeText(getApplicationContext(), "Bu1One == 3", Toast.LENGTH_SHORT).show();
+                } else if (bu1One > 0 && bu2One == 0) {
+                    bu2One = 3;
+                    //    Toast.makeText(getApplicationContext(), "Bu2One == 3", Toast.LENGTH_SHORT).show();
+                } else if (bu1One > 0 && bu2One > 0 && bu3One == 0) {
+                    bu3One = 3;
+
+                }
+                if (bu1One > 0 && bu2One > 0 && bu3One > 0) {
+                    //// TODO: 15-09-25 Make function that will deal with checking if the code entered is correct.
+                    checkCode();
+                }
 
 
             }
@@ -184,48 +187,52 @@ public class arcade extends ActionBarActivity {
     }
 
     public void checkCode() {
+            green+=10;
+
+        final RelativeLayout rl = (RelativeLayout) findViewById(R.id.guiLayout);
 
         int x = Integer.parseInt(code11);
         int y = Integer.parseInt(code12);
         int z = Integer.parseInt(code13);
         if (bu1One == x && bu2One == y && bu3One == z) {
-            Toast.makeText(getApplicationContext(), "Congrats you got the code 1,2,3 correct!, the code will now be reset so that you can find another one!", Toast.LENGTH_SHORT).show();
+        //    Toast.makeText(getApplicationContext(), "Congrats you got the code 1,2,3 correct!, the code will now be reset so that you can find another one!", Toast.LENGTH_SHORT).show();
+            rl.setBackgroundColor(Color.rgb(0, green ,0));
             resetCode();
+
+        } else {
+            Toast.makeText(getApplicationContext(), "Wrong, sorry", Toast.LENGTH_SHORT).show();
+            bu1One = 0;
+            bu2One = 0;
+            bu3One = 0;
+            bu1Clicked = false;
+            bu2Clicked = false;
+            bu3Clicked = false;
+            rl.setBackgroundColor(Color.rgb(0, 0 ,0));
         }
-/**
-        if (code1temp == "1,2,3") {
-            int supo1 = 1;
-            int supo2 = 2;
-            int supo3 = 3;
 
-            if (bu1One == supo1 && bu2One == supo2 && bu3One == supo3) {
-                Toast.makeText(getApplicationContext(), "Congrats you got the code 1,2,3 correct!, the code will now be reset so that you can find another one!", Toast.LENGTH_SHORT).show();
-resetCode();
-            } else {}
-        }
-
-        if (code1temp == "3,2,1") {
-            int supo1 = 3;
-            int supo2 = 2;
-            int supo3 = 1;
-
-            if (bu1One == supo1 && bu2One == supo2 && bu3One == supo3) {
-                Toast.makeText(getApplicationContext(), "Congrats you got the code 3,2,1 correct!", Toast.LENGTH_SHORT).show();
-
-            } else {
-            }
-        }
-*/
 
     }
 
     public void resetCode() {
+
         bu1One = 0;
         bu2One = 0;
         bu3One = 0;
         bu1Clicked = false;
         bu2Clicked = false;
         bu3Clicked = false;
+        choosePass();
+    }
+
+    public void choosePass() {
+        code1temp = code[new Random().nextInt(code.length)]; //choose code
+        code1Decode = code1temp.split(","); //split the code into seperate objects
+        codes = code1Decode.toString(); // turn into strings
+        code11 = code1Decode[0];
+        code12 = code1Decode[1];
+        code13 = code1Decode[2];
+        String p = code11 + code12 + code13;
+     //   Toast.makeText(getApplicationContext(), p, Toast.LENGTH_SHORT).show();
     }
 
 }
